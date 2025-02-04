39cb1ae6a4ed18c2c17244ba96335fa118a190a7
Alex P <apputtre@gmail.com>
2025-02-04 17:59:42 -0500

    Make status level initially blank

--- RandomLevelGeneratorDemo/obj/Debug/net8.0-windows/RandomLevelGeneratorDemo.AssemblyInfo.cs
+++ RandomLevelGeneratorDemo/obj/Debug/net8.0-windows/RandomLevelGeneratorDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RandomLevelGeneratorDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f341440671093abed5758f9c7452bb231b8302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58c9d4b1a5d86f4fb58b9c3b0244d6fd29b0cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RandomLevelGeneratorDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RandomLevelGeneratorDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
